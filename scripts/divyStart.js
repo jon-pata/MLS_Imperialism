0e4fe24539c7894fc1bfc3c6d23db0b2e463f4a3
JonathanPata <jon.pata@gmail.com>
2021-04-11 17:01:06 -0400

    updated Teams.json to include coordinates, to use in divystart

--- scripts/divyStart.js
+++ scripts/divyStart.js
@@ -3,6 +3,8 @@
 // const d3 = require('d3')
 // const mathjs = require('math.js')
 const fetch = require('node-fetch')
+const counties = require('../Resources/test/gz_2010_us_050_00_500k.json')
+const teams = require('../Resources/test/Teams.json')
 
 if(process.argv.length == 2){
   main(process.argv);
@@ -12,13 +14,52 @@ if(process.argv.length == 2){
 
 function main(args){
   console.log(args)
-  let url = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
-  let settings = { method: "Get" };
+  getAllCentroids();
+}
 
-  fetch(url, settings)
-      .then(res => res.json())
-      .then((json) => {
-          // do something with JSON
-          console.log(json)
-      });
+function getAllCentroids(){
+  let features = counties.features;
+
+  for (let feature of features){
+    let geometry = feature.geometry
+    console.log(feature.properties.NAME);
+
+    let centroid = geometry.type == 'Polygon' ? getPolygonCentroid(geometry.coordinates[0]) : getMultiPolygonCentroid(geometry.coordinates)
+
+    console.log(centroid)
+  }
+}
+
+function getPolygonCentroid(points){
+  let centroid = []
+  let clong = 0;
+  let clat = 0;
+  for(let i = 0; i<points.length;i++){
+    let point = points[i]
+    let long = parseFloat(point[0])
+    let lat = parseFloat(point[1])
+    clat += lat
+    clong += long
+  }
+  clat /= points.length
+  clong /= points.length
+  centroid.push(clong)
+  centroid.push(clat)
+  return centroid
+}
+
+function getMultiPolygonCentroid(polys){
+  let centroid = []
+  let clong = 0;
+  let clat = 0;
+  for(let poly of polys){
+    let polyCentroid = getPolygonCentroid(poly[0])
+    clong += polyCentroid[0]
+    clat += polyCentroid[1]
+  }
+  clong /= polys.length
+  clat /= polys.length
+  centroid.push(clong)
+  centroid.push(clat)
+  return centroid
 }
